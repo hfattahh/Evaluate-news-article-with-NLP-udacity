8f23f81b0f5684405a2655618fc10d366291ab6b
Hassan Khodeir <35682564+hfattahh@users.noreply.github.com>
2020-05-23 18:35:14 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,34 +1,34 @@
 const dotenv = require('dotenv');
-dotenv.config();
-
 var path = require('path');
 const aylien = require("aylien_textapi");
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const bodyParser = require('body-parser');
+
+dotenv.config();
 // set aylien API credentias
 const textapi = new aylien({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY,
   });
-  console.log("envAPI_kEY = " + process.env.API_KEY);
+console.log("envAPI_kEY = " + process.env.API_KEY);
 
-const express = require('express');
-const mockAPIResponse = require('./mockAPI.js');
+
 //Set up instance of an app
 const app = express();
 app.use(express.static('dist'));
-// console.log(__dirname); // to print directory name
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+console.log(__dirname); 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 });
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Port 8080 is running...')
-});
-
+const server = app.listen(8080, ()=>{console.log(`running on localhost: 8080`)});
 //Post route
-// app.post('/analysis', addData);
+app.post('/analysis', addData);
     
 const addData = (req, res)=>{
   
@@ -44,7 +44,7 @@ const addData = (req, res)=>{
     );
         
 }
-/* // was for test only
+/* // just for test only
 textapi.sentiment({
     url: 'https://www.imdb.com/title/tt7286456/',
     mode: 'article' 
