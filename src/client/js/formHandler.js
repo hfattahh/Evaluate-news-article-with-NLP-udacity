eeecbc8edc676281bbf70814826d9f06bcf35b82
Hassan Khodeir <35682564+hfattahh@users.noreply.github.com>
2020-05-23 17:04:57 +0200

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,26 +1,31 @@
-
 const handleSubmit = async(event) => {
     event.preventDefault()
     //  get input url
     let input_url = document.getElementById('url').value
     console.log("clicked");
-    client.validURL(input_url);
+    let validation_value = client.validURL(input_url);
     console.log("::: Form Submitted :::");
 
-    fetch('http://localhost:8080/analysis',{
-        method: 'POST', 
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({url:input_url}),
-    })
-    .then(res => res.json())
-    .then((res) => {
-        // print for debugging
-        console.log(res);
-        document.getElementById('results').innerHTML = `Polarity: ${res.polarity} <br> Subjectivity: ${res.subjectivity} <br> Polarity Confidence: ${res.polarity_confidence} <br> Subjectivity Confidence: ${res.subjectivity_confidence} <br> Text: ${res.text}`;
-    })
+    if(validation_value ){
+        fetch('http://localhost:8080/analysis',{
+            method: 'POST', 
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({url:input_url}),
+        })
+        .then(res => res.json())
+        .then((res) => {
+            // print for debugging
+            console.log(res);
+            document.getElementById('results').innerHTML = `Polarity: ${res.polarity} <br> Subjectivity: ${res.subjectivity} <br> Polarity Confidence: ${res.polarity_confidence} <br> Subjectivity Confidence: ${res.subjectivity_confidence} <br> Text: ${res.text}`;
+        });
+    }
+    else{
+        alert('please enter a valid URL');
+    }
+    console.log("::: End of fromHandler function :::");
 }
 
 export { handleSubmit }
